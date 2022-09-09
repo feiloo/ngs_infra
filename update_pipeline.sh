d1f7086afc1cdfe4acd887da7575e3553106795f
feiloo <105425286+feiloo@users.noreply.github.com>
2022-09-09 14:43:04 +0200

    improve upate script

--- update_pipeline.sh
+++ update_pipeline.sh
@@ -2,12 +2,15 @@
 
 rm -rf /tmp/ngs_release
 mkdir -p /tmp/ngs_release
-rsync -au --remove-source-files ukb2580:/tmp/ngs_pipeline_build/meson-dist/*.tar.xz /tmp/ngs_release/
+rsync -au --remove-source-files ukb2580:/tmp/ngs_pipeline_build/meson-dist/*.tar.xz /tmp/ngs_release/ || echo no release found && exit 1
 
 cd /tmp/ngs_release
-tar xvf *.tar.xz
+fname=$(ls)
 
-cd $(ls)
+tar xvf "$fname"
+rm "$fname"
+
+cd $(basename "$fname" .tar.xz)
 
 meson setup /tmp/ngs_release_build
 cd /tmp/ngs_release_build
